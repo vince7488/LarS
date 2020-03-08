752c84eba5d23beb09102476e806c53233bcf95d
vince7488 <vernard.mercader@gmail.com>
2020-03-07 17:49:42 -0800

    workaround for getting the post $id for post.blade.php (note that $postId does not work because it's not defined for getPost)

--- resources/views/main/post.blade.php
+++ resources/views/main/post.blade.php
@@ -2,6 +2,7 @@
 
 @section('MainContainer')
     @if (!empty($post))
+
     <article class="container" style="border:1px #666 solid; border-radius: 15px;padding:10px;">
         <div class="row">
             <hgroup class="col-md-12">
@@ -14,16 +15,22 @@
             </div>
         </div>
         <div class="row justify-content-end">
-            <a href=" {{ route('admin.edit',['id' => $post['id']]) }} " class="btn btn-link" rel="bookmark">Edit</a>
-            <?php //array_search($post,$posts)]
+            <a href=" {{ route('admin.edit',['id' => substr("$_SERVER[REQUEST_URI]", -1)]) }} " class="btn btn-link" rel="bookmark">Edit</a>
+            <?php //How?
+                  //array_search($post,$posts)]
                   //route('admin.edit',['id' => $post['id']])
                   
-                  //$post['title']
-                  //$post['content']
                   //route('admin.edit',['id' => $postId])
+                  //route('admin.edit',['id' => array_search($post,$posts)])
+
+                  //This:
+                  //$arrayIndex = array_search($post['title'], $array) //takes the post title name and outputs the array index number
             ?>
         </div>
     </article>
+
+    {{ var_dump($post) }}<br/>
+
     @else
         <span class="alert alert-error">Error. No posts.</span>
     @endif
